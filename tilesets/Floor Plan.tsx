af41ba4fb4e02ff655f30cc7eb994e4348c672dc
Ross Ritchey <ross@assistedinnovations.com>
2024-06-22 09:05:06 -0400

    Initial map updates. Still needs finished - but need to test

---
+++ tilesets/Floor Plan.tsx
@@ -0,0 +1,10 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="Floor Plan" tilewidth="1010" tileheight="756" tilecount="2" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="1010" height="755" source="image001.png"/>
+ </tile>
+ <tile id="1">
+  <image width="1002" height="756" source="image002.png"/>
+ </tile>
+</tileset>
