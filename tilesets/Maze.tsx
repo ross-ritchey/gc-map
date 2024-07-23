891a8923caac18c686a8ea53d488e663111ec1b4
Ross Ritchey <ross@assistedinnovations.com>
2024-07-23 16:45:29 -0400

    Add Maze

---
+++ tilesets/Maze.tsx
@@ -0,0 +1,7 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="Maze" tilewidth="692" tileheight="666" tilecount="1" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="692" height="666" source="maze.png"/>
+ </tile>
+</tileset>
